fe71b96bcbcf7b64cf4338cc62553b3cb5c8bc63
Somebody Who Exists <texlovesdogs@gmail.com>
2025-11-17 17:03:29 -0500

    testing out image stuff

---
+++ Final_Project/htdocs/js/script.js
@@ -0,0 +1,4 @@
+const chessSquare = document.getElementById(".chess-square");
+
+const newImage = document.createElement("img");
+newImage.src = "https://static.wikia.nocookie.net/characters/images/f/fa/Michael_Myers.jpg/revision/latest?cb=20160924111808";
