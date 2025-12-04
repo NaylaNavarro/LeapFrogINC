ec6cd8edb2c3fa9666c35f05c1f6406cf4e7e7f9
Avab <biegela@purdue.edu>
2025-12-04 15:59:18 -0500

    / not \

--- Final_Project/htdocs/js/script.js
+++ Final_Project/htdocs/js/script.js
@@ -1,7 +1,7 @@
-const chessSquare = document.getElementById(".chess-square");
+const chessSquare = document.getElementById("chess-square");
 
 const newImage = document.createElement("img");
-newImage.src = "118 Final Project\LeapFrogINC\Final_Project\htdocs\Images\Chess_bdt60.png";
+newImage.src = "Images/Chess_bdt60.png";
 document.querySelector(".chess-square").addEventListener("click", function(){
     this.appendChild(newImage);
 });
