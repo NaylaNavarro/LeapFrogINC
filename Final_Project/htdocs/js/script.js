09347287697fdb992ff2e1c871119d4f17ce8a8c
Tex <texlovesdogs@gmail.com>
2025-12-05 15:54:44 -0500

    js board framework added

--- Final_Project/htdocs/js/script.js
+++ Final_Project/htdocs/js/script.js
@@ -1,9 +1,20 @@
-// document.addEventListener("DOMContentLoaded", (event) => {
-//     const chessSquare = document.getElementById("chess-square");
-
-// const newImage = document.createElement("img");
-// newImage.src = "Images/Chess_bdt60.png";
-// document.querySelector(".chess-square").addEventListener("click", function() {
-//     this.appendChild(newImage);
-//     });
-// });
+// 8 rows, 8 columns
+//uppercase = black pieces
+//lowercase = white pieces
+let board = [
+    ["r","n","b","k","q","b","n","r"],  // row 0 (White back rank)
+    ["p","p","p","p","p","p","p","p"],  // row 1
+    ["","","","","","","",""],          // row 2
+    ["","","","","","","",""],          // row 3
+    ["","","","","","","",""],          // row 4
+    ["","","","","","","",""],          // row 5
+    ["P","P","P","P","P","P","P","P"],  // row 6
+    ["R","N","B","K","Q","B","N","R"]   // row 7 (Black back rank)
+  ];
+  
+const blackPawn = document.querySelectorAll(".aBPawn");
+blackPawn.forEach((pawn) => {
+    pawn.addEventListener("click", () => {
+        alert("Black Pawn clicked!");
+    });
+});
